33ba0077fa34a78c7f5120fb9a7766358d1347c3
jiangshijun <shijun.jiang@patsnap-qa.local>
2017-09-23 19:42:11 +0800

    commit code fro test.

--- struct_detect/chapter1/src/main/java/org/smart4j/chapter1/HelloServlet.java
+++ struct_detect/chapter1/src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -14,7 +14,7 @@ import java.util.Date;
 public class HelloServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.printf("servlet is running...");
+        System.out.printf("servlet is running......");
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:MM:ss");
         String currentDate = df.format(new Date());
         req.setAttribute("currentDate",currentDate);
